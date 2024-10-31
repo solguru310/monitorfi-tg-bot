e82b9a84bcabcfec9ad1219067e83e1f3a2d9469
SolGuru310 <147515701+solguru310@users.noreply.github.com>
2024-10-31 15:07:49 +0500

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 export const SOLANA_NETWORK = "mainnet-beta";
 
-export const BOT_TOKEN = "7934382543:AAFRNvbM5nL-fglceKeoOglwz5wPLy_AQ6A";
+export const BOT_TOKEN = "Bot_Token";  
 
 export const Raydium_AMM_ADDRESS = "675kPX9MHTjS2zt1qfr1NYHuzeLXfQM9H24wFSUt1Mp8";
 
